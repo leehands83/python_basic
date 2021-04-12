40bfbfb029dd462587d00460616b900ba0c57ee8
nts_leehands <nhlee@nanots.co.kr>
2021-04-12 13:50:51 +0900

    First_init

---
+++ sample.py
@@ -0,0 +1,12 @@
+anmimal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3 ''' hello ''' 
+
+''' ctrl + + / 영역 주석 '''
+print("우리집 "+anmimal+"의 이름은"+name+"이에요")
+print(name+"는"+str(age)+"살이며"+hobby+"를 좋아해요")
+print(name+"는 어른일까요?"+str(is_adult))
+
+
